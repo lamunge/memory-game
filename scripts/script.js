7a78eba6f7ee87d836d5a9d0ed797f77d43e9656
lamunge <anna.redgrave93@gmail.com>
2018-07-10 09:09:40 -0400

    added card art and matching functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,58 @@
+//display the current number of matches
+numMatches = 0;
+function updateScore() {
+    document.getElementById('matches').innerHTML = "Matches: " + numMatches;
+}
+updateScore();
+
+//initially shuffle the board
+shuffle();
+
 //add "flipped" class to clicked card
 function flip(elem) {
     elem.classList.add('flipped');
+    const flippedCards = document.getElementsByClassName('flipped');
+
+    //if there are two cards flipped over, check if they're the same card (after showing them to the user for 1sec)
+    if (flippedCards.length === 2) {
+        //for some reason, we can't pass functions with parameters to settimeout, hence the anonymous function
+        window.setTimeout(function () {
+            compareCards(flippedCards);
+        }, 1000);
+    }        
+}
+
+function compareCards(cards) {    
+    card1 = cards[0].getElementsByClassName('back')[0].children[0].classList[0];
+    card2 = cards[1].getElementsByClassName('back')[0].children[0].classList[0];
+    if (card1 === card2) {
+        numMatches++;
+        updateScore(); 
+        for (var i = cards.length - 1; i >= 0; i--) {
+            cards[i].classList.add('matched');
+            cards[i].classList.remove('flipped');                       
+        }
+    }
+    else {
+        for (var i = cards.length - 1; i >= 0; i--) {
+            cards[i].classList.remove('flipped');
+        }
+    }    
 }
 
 //shuffles game board by storing each card's outerHTML in javascript array and randomly adding them back to the board
 function shuffle() {
-    const board = document.getElementById("game-board");
-    let cards = board.children; //as HTMLCollection object
+    //flip back over all flipped cards
+    const flippedCards = document.getElementsByClassName('flipped');
+    for (var i = flippedCards.length - 1; i >= 0; i--) {
+        flippedCards[i].classList.remove('flipped');
+    }
 
-    console.log(cards.length);
+    const board = document.getElementById('game-board');
+    let cards = board.children; //as HTMLCollection object - a live list
 
-    //add back children randomly
-    //ANNA: cards is a live list, meaning you can't remove elements without
-    //them  being removed from the DOM. Figure out how to shuffle them in
-    //a way that makes sure you don't duplicate them
+    //append children randomly (yes we never removed them, but that's ok bc they only appear once each in cards)
     for (var i = cards.length; i >= 0; i--) {
-        board.appendChild(cards[Math.random() * i | 0]);
+        board.appendChild(cards[Math.random() * i | 0]); //bitwise OR operator | is basically a floor function: 1.9 | 0 => 1
     }
 }
