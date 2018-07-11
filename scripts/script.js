3914b20069033e6e2eea7dc2f2cb6628ce39c3aa
lamunge <anna.redgrave93@gmail.com>
2018-07-10 23:48:49 -0400

    added some background images and game timer

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,54 @@
-//display the current number of matches
-numMatches = 0;
-function updateScore() {
-    document.getElementById('matches').innerHTML = "Matches: " + numMatches;
-}
-updateScore();
+//global variable for the current number of matches
+let numMatches = 0;
 
-//initially shuffle the board
-shuffle();
+//global variables for start and end times of timer
+let startTime, endTime;
+
+//adds 1 to score and checks if you've gotten all 12 matches
+function updateScore() {
+    numMatches++;
+    if (numMatches === 12) {
+        //stop timer for this round
+        endTime = new Date();
+        timeStars = getTimeAndStars(startTime, endTime);
+        if (confirm("You won! It took you " + timeStars[0] + " seconds. Your rating is " + timeStars[1] + " stars. Play again?")) {
+            restart();
+        }
+        else {
+            alert("Well then...");
+            alert("Guess we're done here...");
+        }
+    }
+}
+
+function getTimeAndStars (start, end) {
+    const time = Math.round((end - start)/1000);
+    let stars;
+    if (time < 60) {
+        stars = 5;
+    }    
+    if ((time >= 60) && (time < 70)) {
+        stars = 4;
+    }
+    if ((time >= 70) && (time < 80)) {
+        stars = 3;
+    }
+    if ((time >= 80) && (time < 90)) {
+        stars = 2;
+    }
+    if ((time >= 90) && (time < 100)) {
+        stars = 1;
+    }
+    if (time >= 100) {
+        stars = 0;
+    }
+        
+    
+    return [time, stars];
+}
+
+//initially restart the board
+restart();
 
 //add "flipped" class to clicked card
 function flip(elem) {
@@ -15,9 +57,24 @@ function flip(elem) {
 
     //if there are two cards flipped over, check if they're the same card (after showing them to the user for 1sec)
     if (flippedCards.length === 2) {
+        //temporarily disable click events during the timeout that's about to happen
+        cards = document.getElementsByClassName('card');
+        for (var i = cards.length - 1; i >= 0; i--) {
+            cards[i].onclick = null;
+        }
+
         //for some reason, we can't pass functions with parameters to settimeout, hence the anonymous function
         window.setTimeout(function () {
             compareCards(flippedCards);
+        }, 1000);
+
+        //reenable click events (hope this is the right place to add this back in...)
+        window.setTimeout(function () {
+            for (var i = cards.length - 1; i >= 0; i--) {
+                cards[i].onclick = function () {
+                    flip(this);
+                }
+            }
         }, 1000);
     }        
 }
@@ -26,7 +83,6 @@ function compareCards(cards) {
     card1 = cards[0].getElementsByClassName('back')[0].children[0].classList[0];
     card2 = cards[1].getElementsByClassName('back')[0].children[0].classList[0];
     if (card1 === card2) {
-        numMatches++;
         updateScore(); 
         for (var i = cards.length - 1; i >= 0; i--) {
             cards[i].classList.add('matched');
@@ -40,12 +96,18 @@ function compareCards(cards) {
     }    
 }
 
-//shuffles game board by storing each card's outerHTML in javascript array and randomly adding them back to the board
-function shuffle() {
+//restarts game board by storing each card's outerHTML in javascript array and randomly adding them back to the board
+function restart() {
+    numMatches = 0;
+
+    //begin the timer for this round
+    startTime = new Date();
+
     //flip back over all flipped cards
-    const flippedCards = document.getElementsByClassName('flipped');
+    const flippedCards = document.querySelectorAll('.flipped, .matched');
     for (var i = flippedCards.length - 1; i >= 0; i--) {
         flippedCards[i].classList.remove('flipped');
+        flippedCards[i].classList.remove('matched');        
     }
 
     const board = document.getElementById('game-board');
